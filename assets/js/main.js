9f77c87849697b1a2180078b969309cf2054bf6c
pauloaguiar <pauloaguiar@live.com>
2019-02-24 20:47:07 -0500

    0.0.2 - build pagination system, added a second view; added some animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,29 @@
 "use strict";
 /*
     Paulo Aguair - 2019
- */
+*/
 var Main = /** @class */ (function () {
     function Main() {
         this.buttonLock = false;
         this.contactFormToggleButton = document.getElementById('contact-button');
         this.contactFormContainer = document.getElementById('contact-form-container');
+        this.contactLabel = document.getElementById('contact-label');
+        this.paginationContainer = document.getElementById('pagination-container');
         this.setupEvents();
+        this.generatePaginationContent();
     }
     Main.prototype.setupEvents = function () {
         var _this = this;
+        this.contactFormToggleButton.onmouseenter = function () {
+            if (!_this.contactFormContainer.classList.contains('open')) {
+                _this.contactLabel.classList.add('shown');
+            }
+        };
+        this.contactFormToggleButton.onmouseleave = function () {
+            if (!_this.contactFormContainer.classList.contains('open')) {
+                _this.contactLabel.classList.remove('shown');
+            }
+        };
         this.contactFormToggleButton.onclick = function () {
             _this.contactFormContainer.classList.toggle('open');
         };
@@ -19,6 +32,25 @@ var Main = /** @class */ (function () {
                 // this.contactFormContainer.classList.toggle('open');
             }
         };
+        window.onscroll = this.scrollEventHandler;
+    };
+    Main.prototype.generatePaginationContent = function () {
+        var amountOfViews = document.getElementsByClassName('main-area').length;
+        var listItem = "<li class='pag-item'></li>";
+        for (var i = 0; i < amountOfViews; i++) {
+            this.paginationContainer.innerHTML += listItem;
+        }
+    };
+    Main.prototype.scrollEventHandler = function () {
+        var scrollPos = window.pageYOffset;
+        var heightOfView = document.getElementsByClassName('main-area')[0].getBoundingClientRect().height;
+        var whichIsShown = Math.floor(scrollPos / heightOfView);
+        var paginationElement = document.getElementsByClassName('pag-item')[whichIsShown];
+        var allPagItems = document.getElementsByClassName('pag-item');
+        for (var i = 0; i < allPagItems.length; i++) {
+            document.getElementsByClassName('pag-item')[i].classList.remove('active');
+        }
+        paginationElement.classList.add('active');
     };
     return Main;
 }());
--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -1,20 +1,37 @@
 /*
     Paulo Aguair - 2019
- */
+*/
 
 class Main {
     private buttonLock: boolean;
     private contactFormToggleButton: HTMLElement;
     private contactFormContainer: HTMLElement;
+    private contactLabel: HTMLElement;
+    private paginationContainer: HTMLElement;
 
     constructor() {
         this.buttonLock = false;
         this.contactFormToggleButton = <HTMLElement>document.getElementById('contact-button');
         this.contactFormContainer = <HTMLElement>document.getElementById('contact-form-container');
+        this.contactLabel = <HTMLElement>document.getElementById('contact-label');
+        this.paginationContainer = <HTMLElement>document.getElementById('pagination-container');
         this.setupEvents();
+        this.generatePaginationContent();
     }
 
     private setupEvents(): void {
+        this.contactFormToggleButton.onmouseenter = () => {
+            if (!this.contactFormContainer.classList.contains('open')) {
+                this.contactLabel.classList.add('shown');
+            }
+        };
+
+        this.contactFormToggleButton.onmouseleave = () => {
+            if (!this.contactFormContainer.classList.contains('open')) {
+                this.contactLabel.classList.remove('shown');
+            }
+        };
+
         this.contactFormToggleButton.onclick = () => {
             this.contactFormContainer.classList.toggle('open');
         };
@@ -24,6 +41,31 @@ class Main {
                 // this.contactFormContainer.classList.toggle('open');
             }
         };
+
+        window.onscroll = this.scrollEventHandler;
+    }
+
+    private generatePaginationContent(): void {
+        const amountOfViews = document.getElementsByClassName('main-area').length;
+        const listItem = "<li class='pag-item'></li>";
+
+        for (let i = 0; i < amountOfViews; i++) {
+            this.paginationContainer.innerHTML += listItem;
+        }
+    }
+
+    private scrollEventHandler(): void {
+        const scrollPos = window.pageYOffset;
+        const heightOfView = document.getElementsByClassName('main-area')[0].getBoundingClientRect().height;
+        const whichIsShown = Math.floor(scrollPos / heightOfView);
+        const paginationElement = document.getElementsByClassName('pag-item')[whichIsShown];
+        const allPagItems = document.getElementsByClassName('pag-item');
+
+        for (let i = 0; i < allPagItems.length ; i++) {
+            document.getElementsByClassName('pag-item')[i].classList.remove('active');
+        }
+
+        paginationElement.classList.add('active');
     }
 }
 
